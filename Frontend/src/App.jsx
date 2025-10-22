14806209dadd91d5d3da2eb8e887b2eb0310639d
Ashutosh maurya <ashutoshmauriyacse@gmail.com>
2025-10-22 12:20:02 +0530

    basic infra

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='flex items-center justify-center flex-col'>
+        <img className='w-80' src="/logo.png" alt="" />
+        <h1 className='text-6xl font-medium p-5 text-blue-600 border rounded-xl hover:bg-blue-600 hover:text-white'>Abroad Aura</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
